9b91209c6f845d2f3320bb7f65a0174e0a86f7a6
ausboss <102309304+ausboss@users.noreply.github.com>
2023-02-11 04:05:02 -0600

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,8 +12,9 @@ from dotenv import load_dotenv
 load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 ENDPOINT = os.getenv("ENDPOINT")
+PERIOD_IGNORE = os.getenv("PERIOD_IGNORE")
 # Set this to True if you want messages starting with . to be ignored
-period_ignore = False
+
 DEBUG = True
 def split_text(text):
     parts = re.split(r'\n[a-zA-Z]', text)
@@ -144,7 +145,7 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
 @client.event
 async def on_message(message):
-    if period_ignore and message.content.startswith("."):
+    if PERIOD_IGNORE and message.content.startswith("."):
         return
     else:
         # global first_message
