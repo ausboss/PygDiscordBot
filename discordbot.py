434e4271732add977e32c348c020c182d545c5a2
Sen <nevyn202@outlook.com>
2023-02-19 13:08:26 -0600

    The Tavernlike Update
    Changes:
    - Added Custom Generation Settings
    - Added Chat Logs
    - Added Example Dialogue
    - Better 'You:' token detection and cleaning
    - Shortened default token output to lower response time.

--- discordbot.py
+++ discordbot.py
@@ -12,10 +12,14 @@ from discord.ext import commands
 # get .env variables
 load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
 ENDPOINT = os.getenv("ENDPOINT")
 PERIOD_IGNORE = os.getenv("PERIOD_IGNORE")
+SAVE_CHATS = os.getenv("SAVE_CHATS")
+SEND_GREETING = os.getenv("SEND_GREETING")
 def split_text(text):
     parts = re.split(r'\n[a-zA-Z]', text)
+    parts = [part for part in parts if not part.startswith("You:")]
     return parts
 def upload_character(json_file, img, tavern=False):
     json_file = json_file if type(json_file) == str else json_file.decode('utf-8')
@@ -47,29 +51,31 @@ def upload_tavern_character(img, name1, name2):
 def get_prompt(conversation_history, user, text):
     return {
         "prompt": conversation_history + f"{user}: {text}\n{char_name}:",
-        "use_story": False,
-        "use_memory": False,
-        "use_authors_note": False,
-        "use_world_info": False,
-        "max_context_length": 1818,
-        "max_length": 180,
-        "rep_pen": 1.03,
-        "rep_pen_range": 1024,
-        "rep_pen_slope": 0.9,
-        "temperature": 0.98,
-        "tfs": 0.9,
-        "top_a": 0,
-        "top_k": 0,
-        "top_p": 0.9,
-        "typical": 1,
-        "sampler_order": [6, 0, 1, 2, 3, 4, 5],
-        "frmttriminc": True,
-        "frmtrmblln": True
+        "use_story": setting['use_story'],
+        "use_memory": setting['use_memory'],
+        "use_authors_note": setting['use_authors_note'],
+        "use_world_info": setting['use_world_info'],
+        "max_context_length": setting['max_context_length'],
+        "max_length": setting['max_length'],
+        "rep_pen": setting['rep_pen'],
+        "rep_pen_range": setting['rep_pen_range'],
+        "rep_pen_slope": setting['rep_pen_slope'],
+        "temperature": setting['temperature'],
+        "tfs": setting['tfs'],
+        "top_a": setting['top_a'],
+        "top_k": setting['top_k'],
+        "top_p": setting['top_p'],
+        "typical": setting['typical'],
+        "sampler_order": setting['sampler_order'],
+        "frmttriminc": setting['frmttriminc'],
+        "frmtrmblln": setting['frmtrmblln']
     }
 
 characters_folder = 'Characters'
 cards_folder = 'Cards'
+settings_folder = 'Settings'
 characters = []
+settings = []
 # Check the Cards folder for cards and convert them to characters
 try:
     for filename in os.listdir(cards_folder):
@@ -81,7 +87,7 @@ try:
                 tavern_character_data = upload_tavern_character(img, name1, name2)
             with open(os.path.join(characters_folder, tavern_character_data + '.json')) as read_file:
                 character_data = json.load(read_file)
-                # characters.append(character_data)
+                characters.append(character_data)
             read_file.close()
             os.rename(os.path.join(cards_folder, filename), os.path.join(cards_folder, 'Converted', filename))
 except:
@@ -107,18 +113,49 @@ data = characters[selected_char]
 # Get the character name, greeting, and image
 char_name = data["char_name"]
 char_greeting = data["char_greeting"]
-char_dialogue = data["char_greeting"]
+char_dialogue = data["example_dialogue"]
+char_dialogue = char_dialogue.replace('"', "")
 char_image = data.get("char_image")
-
+# Get the settings for the generation.
+for filename in os.listdir(settings_folder):
+    if filename.endswith('.settings'):
+        with open(os.path.join(settings_folder, filename)) as read_file:
+            trimed_name = filename.replace('.settings', '')
+            generation_settings = json.load(read_file)
+            generation_settings['setting_name'] = trimed_name
+            generation_settings['use_story'] = generation_settings.get('use_story', False)
+            generation_settings['use_memory'] = generation_settings.get('use_memory', False)
+            generation_settings['use_authors_note'] = generation_settings.get('use_authors_note', False)
+            generation_settings['use_world_info'] = generation_settings.get('use_world_info', False)
+            generation_settings['max_context_length'] = generation_settings.get('max_length', 1818)
+            generation_settings['max_length'] = generation_settings.get('genamt', 60)
+            generation_settings['rep_pen'] = generation_settings.get('rep_pen', 1.01)
+            generation_settings['rep_pen_range'] = generation_settings.get('rep_pen_range', 1024)
+            generation_settings['rep_pen_slope'] = generation_settings.get('rep_pen_slope', 0.9)
+            generation_settings['temperature'] = generation_settings.get('temperature', 1)
+            generation_settings['tfs'] = generation_settings.get('tfs', 0.9)
+            generation_settings['top_a'] = generation_settings.get('top_a', 0)
+            generation_settings['top_k'] = generation_settings.get('top_k', 40)
+            generation_settings['top_p'] = generation_settings.get('top_p', 0.9)
+            generation_settings['typical'] = generation_settings.get('typical', 1)
+            generation_settings['sampler_order'] = generation_settings.get('sampler_order', [6, 0, 1, 2, 3, 4, 5])
+            generation_settings['frmttriminc'] = generation_settings.get('frmttriminc', True)
+            generation_settings['frmtrmblln'] = generation_settings.get('frmtrmblln', True)
+            settings.append(generation_settings)
+for i, setting in enumerate(settings):
+    print(f"{i+1}. {generation_settings['setting_name']}")
+selected_settings= int(input("Please select a settings file: ")) - 1
+setting = settings[selected_settings]
 num_lines_to_keep = 20
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='/', intents=intents)
 conversation_history = f"{char_name}'s Persona: {data['char_persona']}\n" + \
-                        f"World Scenario: {data['world_scenario']}\n" + \
+                        f"Scenario: {data['world_scenario']}\n" + \
                         f'<START>\n' + \
                         f'{char_dialogue}' + \
                         f'<START>\n' + \
                         f'f"{char_name}: {char_greeting}\n'
+starting_dialogue = char_greeting.replace("{", "").replace("}", "").replace("'", "")
 @bot.event
 async def on_ready():
     try:
@@ -137,27 +174,34 @@ async def on_ready():
             pass
         else:
             raise error
-    print(f'{bot.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord! The bot will use the following channel: ' + CHANNEL_ID)
+    channel = bot.get_channel(int(CHANNEL_ID))
+    if SEND_GREETING and not os.path.exists('Logs/chat_logs.txt') :
+        await channel.send(char_greeting)
+    elif SEND_GREETING:
+        await channel.send(char_greeting)
 @bot.command()
 async def reset(ctx):
     global conversation_history
     conversation_history = f"{char_name}'s Persona: {data['char_persona']}\n" + \
-                            f"World Scenario: {data['world_scenario']}\n" + \
+                            f"Scenario: {data['world_scenario']}\n" + \
                             f'<START>\n' + \
                             f'{char_dialogue}' + \
                             f'<START>\n' + \
                             f'f"{char_name}: {char_greeting}\n'
     await ctx.send("Conversation history has been reset.")
-
 @bot.event
 async def on_message(message):
-    if PERIOD_IGNORE and message.content.startswith(".") and not message.content.startswith("/"):
+    if PERIOD_IGNORE and message.content.startswith(".") and not message.content.startswith("/") and not message.channel.id == CHANNEL_ID:
         return
     else:
         global conversation_history
         if message.author == bot.user:
             return
         your_message = message.content
+        if SAVE_CHATS:
+            with open("Logs/chat_logs.txt", "a") as file:
+                file.write(f"You:{your_message}\n")
         print(f"{message.author.name}:{your_message}")
         prompt = get_prompt(conversation_history,message.author.name, message.content)
         print(prompt)
@@ -166,8 +210,11 @@ async def on_message(message):
             results = response.json()['results']
             text = results[0]['text']
             response_text = split_text(text)[0]
-            await message.channel.send(response_text)
+            if SAVE_CHATS:
+                with open("Logs/chat_logs.txt", "a") as file:
+                    file.write(f'{char_name}: {response_text}\n')
             conversation_history = conversation_history + f'{char_name}: {response_text}\n'
-
+            response_text = response_text.replace(char_name+":", "")
+            await message.channel.send(response_text)
 
 bot.run(DISCORD_BOT_TOKEN)
